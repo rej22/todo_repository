6c05aeae4384eaa44a363c38ad9c5d09c31930ba
rej22 <rejilraj813@outlook.com>
2023-03-08 12:10:29 +0530

    a new field has been added to the model Task. its a text field named as taskDescription

--- todofolder01/todo_folder_01/projecttodo01/todoapp01/models.py
+++ todofolder01/todo_folder_01/projecttodo01/todoapp01/models.py
@@ -5,6 +5,7 @@ class Task(models.Model):
     taskName = models.CharField(max_length= 300)
     taskPriority = models.IntegerField()
     taskDate = models.DateField()
+    taskDescription = models.TextField(blank=True)
 
     def __str__(self):
         return self.taskName
